c3fd5d7fe14e8ee9f1d7687be4d9eefcded99fee
Armando <borelliarmando28@gmail.com>
2023-12-30 17:11:06 -0500

    Added function for generating bot pick

--- rust-rps/src/main.rs
+++ rust-rps/src/main.rs
@@ -1,3 +1,17 @@
+use rand::Rng;
+
+
+fn bot_pick() -> &'static str {
+    // Generates random number
+    let mut rng = rand::thread_rng();
+    let random_number: usize = rng.gen_range(0..=2);
+
+    let picks: [&str; 3] = ["Rock", "Paper", "Scissor"];
+    let pick: &str = picks[random_number];
+    return pick;
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", bot_pick());
 }
