0d87fba86ee818b5cd87706b186294f021e8fc8f
Armando <borelliarmando28@gmail.com>
2024-06-01 23:57:26 -0400

    Added completed assembly x64 program

--- rockpaperscissors.asm
+++ rockpaperscissors.asm
@@ -4,6 +4,9 @@
 
 section .data
         pick_prompt db "Rock[R], Paper[P], or Scissors[S]: ", 0
+        is_draw db "Draw", 0
+        win db "W", 0
+        lose db "L", 0
 
 section .text
         global _start
@@ -20,10 +23,12 @@ _start:
         mov rax, 0      ; sys_read
         mov rdi, 1      ; stdout
         mov rsi, pick_input     ; store user input
-        mov rdx, 1
+        mov rdx, 1      ; amount of bytes to read
         syscall
 
         call gen_bot_pick
+
+        call gen_winner
 
         ; exit program
         mov rax, 60     ; sys_exit
@@ -33,13 +38,13 @@ _start:
 gen_bot_pick:
         ; Generate a random number between 0 and 2
         rdrand rax
-        and rax, 0x3    ; Mask to keep the last two bits(0,1,2)
+        and rax, 0x3    ; mask to keep the last two bits(0,1,2)
 
-        cmp rax, 0
+        cmp rax, 0      ; if rock, go to rock
         je rock
-        cmp rax, 1
+        cmp rax, 1      ; if paper, go to paper
         je paper
-        jmp scissors
+        jmp scissors    ; else, go to scissors
 
 rock:
         mov byte [bot_pick], 'r'
@@ -52,3 +57,58 @@ paper:
 scissors:
         mov byte [bot_pick], 's'
         ret
+
+gen_winner:
+        mov rax, [pick_input]   ; user pick
+        mov rbx, [bot_pick]     ; bot pick
+
+        cmp rax, rbx    ; are both picks the same?
+        je draw
+
+        cmp rax, 'r'    ; is user rock?
+        je user_rock
+
+        cmp rax, 'p'    ; is user paper?
+        je user_paper
+
+        cmp rax, 's'    ; is user scissors?
+        je user_scissors
+
+user_rock:
+        cmp rbx, 's'    ; is bot pick scissors?
+        je user_wins
+        jmp bot_wins    ; else bot wins
+
+user_paper:
+        cmp rbx, 'r'    ; is bot pick rock?
+        je user_wins
+        jmp bot_wins    ; else bot wins
+
+user_scissors:
+        cmp rbx, 'p'    ; is bot pick paper?
+        je user_wins
+        jmp bot_wins    ; else bot wins
+
+draw:
+        mov rax, 1      ; sys_write
+        mov rdi, 1      ; file descriptor, stdout
+        mov rsi, is_draw        ; pointer to string
+        mov rdx, 4      ; length of bytes to write
+        syscall
+        ret             ; return
+
+user_wins:
+        mov rax, 1      ; sys_write
+        mov rdi, 1      ; file descriptor, stdout
+        mov rsi, win    ; pointer to string
+        mov rdx, 1      ; length of bytes to write
+        syscall
+        ret             ; return
+
+bot_wins:
+        mov rax, 1      ; sys_write
+        mov rdi, 1      ; file descriptor, stdout
+        mov rsi, lose   ; pointer to string
+        mov rdx, 1      ; length of bytes to write
+        syscall
+        ret             ; return
