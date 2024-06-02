48a3404c286fa03068b878220f973084c63f9e79
Armando <borelliarmando28@gmail.com>
2024-06-01 22:07:13 -0400

    Added code to generate bot pick

--- rockpaperscissors.asm
+++ rockpaperscissors.asm
@@ -1,5 +1,6 @@
 ﻿section .bss
         pick_input resb 1
+        bot_pick resb 1
 
 section .data
         pick_prompt db "Rock[R], Paper[P], or Scissors[S]: ", 0
@@ -22,7 +23,32 @@ _start:
         mov rdx, 1
         syscall
 
+        call gen_bot_pick
+
         ; exit program
         mov rax, 60     ; sys_exit
         xor rdi, rdi    ; clear rdi register and set it to 0
         syscall
+
+gen_bot_pick:
+        ; Generate a random number between 0 and 2
+        rdrand rax
+        and rax, 0x3    ; Mask to keep the last two bits(0,1,2)
+
+        cmp rax, 0
+        je rock
+        cmp rax, 1
+        je paper
+        jmp scissors
+
+rock:
+        mov byte [bot_pick], 'r'
+        ret
+
+paper:
+        mov byte [bot_pick], 'p'
+        ret
+
+scissors:
+        mov byte [bot_pick], 's'
+        ret
